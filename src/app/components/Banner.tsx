093d1039fa595f5fc83298122538aa094b89b713
RodolphoStraub <89853868+RodolphoStraub@users.noreply.github.com>
2024-09-05 18:20:01 -0300

    changes

--- src/app/components/Banner.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Banner.module.css";
-
-export default function Banner() {
-  return (
-    <div id="/" className={styles.all}>
-      <img src="/banner_home.png" alt="banner " />
-    </div>
-  );
-}
