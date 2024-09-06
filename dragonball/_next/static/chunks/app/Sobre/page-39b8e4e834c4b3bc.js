63ec6fc43d161cbe9bdef94b27df9799e9390d29
RodolphoStraub <89853868+RodolphoStraub@users.noreply.github.com>
2024-09-06 11:25:38 -0300

    commit 1

---
+++ dragonball/_next/static/chunks/app/Sobre/page-39b8e4e834c4b3bc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[581],{3891:function(_,n,e){Promise.resolve().then(e.t.bind(e,9010,23))},9010:function(_){_.exports={all:"Sobre_all__WSgY7",sub_all:"Sobre_sub_all__c9RVQ"}}},function(_){_.O(0,[594,971,23,744],function(){return _(_.s=3891)}),_N_E=_.O()}]);
