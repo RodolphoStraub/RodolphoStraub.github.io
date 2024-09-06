fff05e023bb37639384d3376b5ef4f1bfd11a0f6
RodolphoStraub <89853868+RodolphoStraub@users.noreply.github.com>
2024-09-06 01:28:02 -0300

    finalize script

--- fixPaths.js
+++ fixPaths.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const projectName = "graph-it";
 const exportPath = path.join(__dirname, projectName);
@@ -9,31 +9,55 @@ const baseURL = `https://rodolphostraub.github.io/${projectName}`;
 function fixPaths(dir) {
   const files = fs.readdirSync(dir);
 
-  files.forEach(file => {
+  files.forEach((file) => {
     const filePath = path.join(dir, file);
     const stats = fs.statSync(filePath);
 
     // Verifique se o arquivo é um diretório ou arquivo HTML
     if (stats.isDirectory()) {
       fixPaths(filePath); // Recursão para entrar em subpastas
-    } else if (stats.isFile() && file.endsWith('.html')) {
-      let content = fs.readFileSync(filePath, 'utf8');
+    } else if (
+      stats.isFile() &&
+      (file.endsWith(".html") || file.endsWith(".txt"))
+    ) {
+      let content = fs.readFileSync(filePath, "utf8");
 
       // Adicione a base URL aos caminhos normais (exceto aqueles que começam com "/_next" e já contêm "graph-it")
-      content = content.replace(/src="\/(?!_next)(?!graph-it)([^"]*)"/g, `src="${baseURL}/$1"`);
-      content = content.replace(/href="\/(?!_next)(?!graph-it)([^"]*)"/g, `href="${baseURL}/$1"`);
+      content = content.replace(
+        /src="\/(?!_next)(?!graph-it)([^"]*)"/g,
+        `src="${baseURL}/$1"`
+      );
+      content = content.replace(
+        /href="\/(?!_next)(?!graph-it)([^"]*)"/g,
+        `href="${baseURL}/$1"`
+      );
 
       // Adicione a base URL para caminhos dentro de scripts (com \")
-      content = content.replace(/\\"src\\":\\"\/(?!_next)(?!graph-it)([^\\"]*)\\"/g, `\\"src\\":\\"${baseURL}/$1\\"`);
-      content = content.replace(/\\"href\\":\\"\/(?!_next)(?!graph-it)([^\\"]*)\\"/g, `\\"href\\":\\"${baseURL}/$1\\"`);
+      content = content.replace(
+        /\\"src\\":\\"\/(?!_next)(?!graph-it)([^\\"]*)\\"/g,
+        `\\"src\\":\\"${baseURL}/$1\\"`
+      );
+      content = content.replace(
+        /\\"href\\":\\"\/(?!_next)(?!graph-it)([^\\"]*)\\"/g,
+        `\\"href\\":\\"${baseURL}/$1\\"`
+      );
 
-      content = content.replace(new RegExp(`/${projectName}/_next/`, 'g'),`${baseURL}/_next/`)
+      content = content.replace(
+        new RegExp(`/${projectName}/_next/`, "g"),
+        `${baseURL}/_next/`
+      );
+
+      content = content.replace(
+        /src":"\/([^"]*)"/g, 
+        `src":"${baseURL}/$1"`
+      );
+
       // Salve o arquivo com os caminhos corrigidos
-      fs.writeFileSync(filePath, content, 'utf8');
+      fs.writeFileSync(filePath, content, "utf8");
     }
   });
 }
 
 // Chamar a função fixPaths para corrigir os caminhos
 fixPaths(exportPath);
-console.log('Caminhos corrigidos com sucesso!');
+console.log("Caminhos corrigidos com sucesso!");
